7d7bddd89344db0dc7bd538deb7bd48507b34722
Nafiseh <nafiseh_hoseini@ut.ac.ir>
2021-12-15 11:14:16 +0330

    symbol

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+const mystatus = Object.freeze({
+    INFO:Symbol("info"), ERROR:Symbol("error")
+})
 
-function sayHello(h){
-//hi
-    alert (h);
+function log(message, status) {
+
+    switch (status) {
+        case mystatus.INFO:
+            console.log("Info:" + message);
+            break;
+
+        case mystatus.ERROR:
+            console.log("error:" + message);
+            break;
+        default:
+            throw "Error"
+            break;
+    }
+
 }
+console.log(log("hello",mystatus.INFO));
+
+
